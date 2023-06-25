4c8eb8d0c8de4181921943cc9ea8b645b0c08a29
KirillKozlo <kirillkozlogit@gmail.com>
2023-06-25 19:05:12 +0300

    add css mainContent

--- src/components/Main/MainContent/Card/Card.tsx
+++ src/components/Main/MainContent/Card/Card.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import {UniversalButton} from "../../../UniversalButton/UniversalButton";
 import {ExtendedPhonesType} from "../../../../Store/PhoneReducer";
+import s from "./Card.module.css"
 
-type CardPropsType={
-    productParam:ExtendedPhonesType
+type CardPropsType = {
+    productParam: ExtendedPhonesType
 }
 
 
-
-
-export const Card = (props:CardPropsType) => {
-    let {productParam}=props
+export const Card = (props: CardPropsType) => {
+    let {productParam} = props
     return (
-        <div>
-            <span>Сравнить</span>
-            <span>Изображение</span>
-            <span>{`${productParam.brand}+" "+${productParam.model}`}</span>
-            <span>{productParam.price}</span>
-            <span>{productParam.availability}</span>
-            <span>Избраное</span>
-            <span>Купить</span>
-<UniversalButton nameButton={"Купить"} callback={()=>{}}/>
+        <div className={s.card}>
+
+            <img className={s.imgProduct} src="https://alldect.ru/images/stories/virtuemart/product/spektr_308.jpg"
+                 alt="PhoneIMG"/>
+            <div className={s.infoCard}>
+                <span>{`${productParam.brand} ${productParam.model}`}</span>
+                <span>Наличие<input type="checkbox" checked={productParam.availability}/></span> <span>Сравнить<input type="checkbox" checked={false}/></span>
+            </div>
+
+            <div className={s.buyMenuCard}>
+                <span className={s.cardPriceProduct}>Цена: {productParam.price}</span>
+                <span>Избраное<input type="checkbox" checked={false}/></span>
+                <UniversalButton nameButton={"Купить"} callback={() => {
+                }}/></div>
         </div>
     );
 };
--- src/components/Main/MainContent/MainContent.tsx
+++ src/components/Main/MainContent/MainContent.tsx
@@ -2,18 +2,15 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import {ExtendedPhonesType} from "../../../Store/PhoneReducer";
 import {Card} from "./Card/Card";
-
+import s from "./MainContent.module.css"
 
 export const MainContent = () => {
-    let statePhone=useSelector<ExtendedPhonesType[],ExtendedPhonesType[]>(store=>store)
+    let statePhone = useSelector<ExtendedPhonesType[], ExtendedPhonesType[]>(store => store)
     return (
-        <div>
-            {statePhone.map(el=>{
-               return  <Card  productParam={el} />
+        <div className={s.MainContent}>
+            {statePhone.map(el => {
+                return <Card productParam={el}/>
             })}
-            Main content
-            <span>МАПИМ телефоньчики!!!!</span>
-
 
         </div>
     );
