e3eb46d92de9eb4cecda2ce58a502be825afe928
KirillKozlo <kirillkozlogit@gmail.com>
2023-06-29 23:33:15 +0300

    add FilterCurrency

--- src/components/Main/MainNav/MainNav.tsx
+++ src/components/Main/MainNav/MainNav.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import s from "./MainNav.module.css";
 import {InStockFilter} from "./InStockFilter/InStockFilter";
+import {CurrencyFilter} from "./CurrencyFilter/CurrencyFilter";
 
 export const MainNav = () => {
     return (
         <nav className={s.navWrapper}>
             <InStockFilter/>
+            <CurrencyFilter/>
             <span>Фильтр цены и чекбоксы с популярными ценовыми дипозонами</span>
             <span>поиск по модели и чекбоксы с брендом</span>
             <span>Фильтр диагонали экранаа и чекбоксы с популярными диагоналями и селекты </span>
